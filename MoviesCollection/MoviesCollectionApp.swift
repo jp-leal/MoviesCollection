0d73c6c30f1cc3afb56896b174296b505fb5cb5a
joaobug <joaoenfrance@gmail.com>
2024-09-04 10:47:32 -0300

    Initial Commit

---
+++ MoviesCollection/MoviesCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCollectionApp.swift
+//  MoviesCollection
+//
+//  Created by Joao Leal on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
